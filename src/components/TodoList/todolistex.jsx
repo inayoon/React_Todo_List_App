968d91841275582871b0e7f5631496350cb31336
Ina Yoon <yia0319@gmail.com>
2023-08-17 22:41:07 -0400

    4

--- src/components/TodoList/todolistex.jsx
+++ src/components/TodoList/todolistex.jsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 
 
 
-export default function todolistex() {
+export default function todolistex({ filter }) {
   const [todos, setTodos] = useState([
     { id :'123', text: '장보기', status: 'active'},
     { id :'124', text: '준보기', status: 'active'},
@@ -10,10 +10,12 @@ export default function todolistex() {
   const handleAdd =(todo) => setTodos([...todos, todo])
   const handleUpdate =(updated) => setTodos(todos.map((t)=> (t.id ===updated.id? updated : t)))
   const handleDelete =(deleted) => setTodos(todos.filter((t)=>t.id!== deleted.id)) 
+
+  const filtered = getFilteredItems(todos, filter);
   return (
     <section>
       <ul>
-        {todos.map((item) =>(
+        {filtered.map((item) =>(
           <Todo 
             key={item.id} 
             todo={item} 
@@ -24,4 +26,11 @@ export default function todolistex() {
       <Addtodoex onAdd={handleAdd}/>
     </section>
   )
+}
+
+function getFilteredItems(todos, filter){
+  if(filter === 'all'){
+    return todos;
+  }
+  return todos.filter(todo => todo.status ===filter );
 }
